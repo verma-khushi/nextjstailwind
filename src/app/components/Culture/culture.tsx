7293675902926d6d903e5622fc8145ea33d633a6
Khushi Verma <khushiv@geekyants.com>
2023-10-17 12:53:03 +0530

    Culture component fetched through ssr

--- src/app/components/Culture/culture.tsx
+++ src/app/components/Culture/culture.tsx
@@ -3,7 +3,19 @@ import Image from "next/image";
 import ArrowLeft from "../../../../svg-components/ArrowLeft";
 import ArrowRight from "../../../../svg-components/ArrowRight";
 import SliderProvider from "../SliderProvider";
+import data from "./culture.json";
+interface Culture {
+  image: string;
+  title: string;
+  text: string;
+  description: string;
+}
+function getCultureData() {
+  return data;
+}
 export default function Culture() {
+  const CultureData = getCultureData();
+  // console.log("culturedata",CultureData);
   return (
     <div className=" 2xl:container bg-lightorange relative   md:top-[-90px] top-[200px] md:py-20 py-10 px-30 ">
       <SliderProvider>
@@ -24,110 +36,35 @@ export default function Culture() {
 
         <div className="blaze-track-container relative bottom-8 md:right-10">
           <div className="blaze-track">
-            <div className="flex flex-col md:flex-row items-center">
-              <Image
-                src="/sliderimg.png"
-                alt="sliderImage"
-                className="w-full md:w-[650px] px-4 relative md:right-[20px] mx-auto md:order-2"
-                width={300}
-                height={700}
-              />
+            {CultureData.map((data, index) => (
+              <div
+                className="flex flex-col md:flex-row items-center"
+                key={index}
+              >
+                <Image
+                  src={data.image}
+                  alt="sliderImage"
+                  className="w-full md:w-[650px] px-4 relative md:right-[20px] mx-auto md:order-2"
+                  width={300}
+                  height={700}
+                />
 
-              <div className="flex flex-col text-left md:text-left bottom-[18px]  md:relative md:left-[90px]  px-5 md:order-1">
-                <span className="text-base relative xl:bottom-[18px] font-semibold xl:mt-2 mt-4">
-                  A space for appreciation
-                </span>
-                <span className="text-xsm text-lightred md:mt-3 mt-6">
-                  REWARDS AND RECOGNITION
-                </span>
-                <span className="md:text-sm md:w-7/12 mt-4 leading-[28px] text-xsm w-full">
-                  We believe in recognising and celebrating employees
-                  milestones, innovative ideas, and drive to achieve their
-                  goals.
-                </span>
-                <button className="text-left text-sm mt-4 text-blue">
-                  Learn More
-                </button>
+                <div className="flex flex-col text-left md:text-left bottom-[18px]  md:relative md:left-[90px]  px-5 md:order-1">
+                  <span className="text-base relative xl:bottom-[18px] font-semibold xl:mt-2 mt-4">
+                    {data.title}
+                  </span>
+                  <span className="text-xsm text-lightred md:mt-3 mt-6">
+                    {data.text}
+                  </span>
+                  <span className="md:text-sm md:w-7/12 mt-4 leading-[28px] text-xsm w-full">
+                    {data.description}
+                  </span>
+                  <button className="text-left text-sm mt-4 text-blue">
+                    Learn More
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col md:flex-row items-center">
-              <Image
-                src="/sliderimg.png"
-                alt="sliderImage"
-                className="w-full md:w-[650px] px-4 relative md:right-[20px] mx-auto md:order-2"
-                width={300}
-                height={700}
-              />
-
-              <div className="flex flex-col text-left md:text-left bottom-[18px]  md:relative md:left-[90px]  px-5 md:order-1">
-                <span className="text-base relative xl:bottom-[18px] font-semibold xl:mt-2 mt-4">
-                  A space for appreciation
-                </span>
-                <span className="text-xsm text-lightred md:mt-3 mt-6">
-                  REWARDS AND RECOGNITION
-                </span>
-                <span className="md:text-sm md:w-7/12 mt-4 leading-[28px] text-xsm w-full">
-                  We believe in recognising and celebrating employees
-                  milestones, innovative ideas, and drive to achieve their
-                  goals.
-                </span>
-                <button className="text-left text-sm mt-4 text-blue">
-                  Learn More
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-col md:flex-row items-center">
-              <Image
-                src="/sliderimg.png"
-                alt="sliderImage"
-                className="w-full md:w-[650px] px-4 relative md:right-[20px] mx-auto md:order-2"
-                width={300}
-                height={700}
-              />
-
-              <div className="flex flex-col text-left md:text-left bottom-[18px]  md:relative md:left-[90px]  px-5 md:order-1">
-                <span className="text-base relative xl:bottom-[18px] font-semibold xl:mt-2 mt-4">
-                  A space for appreciation
-                </span>
-                <span className="text-xsm text-lightred md:mt-3 mt-6">
-                  REWARDS AND RECOGNITION
-                </span>
-                <span className="md:text-sm md:w-7/12 mt-4 leading-[28px] text-xsm w-full">
-                  We believe in recognising and celebrating employees
-                  milestones, innovative ideas, and drive to achieve their
-                  goals.
-                </span>
-                <button className="text-left text-sm mt-4 text-blue">
-                  Learn More
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-col md:flex-row items-center">
-              <Image
-                src="/sliderimg.png"
-                alt="sliderImage"
-                className="w-full md:w-[650px] px-4 relative md:right-[20px] mx-auto md:order-2"
-                width={300}
-                height={700}
-              />
-
-              <div className="flex flex-col text-left md:text-left bottom-[18px]  md:relative md:left-[90px]  px-5 md:order-1">
-                <span className="text-base relative xl:bottom-[18px] font-semibold xl:mt-2 mt-4">
-                  A space for appreciation
-                </span>
-                <span className="text-xsm text-lightred md:mt-3 mt-6">
-                  REWARDS AND RECOGNITION
-                </span>
-                <span className="md:text-sm md:w-7/12 mt-4 leading-[28px] text-xsm w-full">
-                  We believe in recognising and celebrating employees
-                  milestones, innovative ideas, and drive to achieve their
-                  goals.
-                </span>
-                <button className="text-left text-sm mt-4 text-blue">
-                  Learn More
-                </button>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </SliderProvider>
