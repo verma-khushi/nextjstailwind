9f77be8d01524d73e3d7fe2272e518e19827b288
Khushi Verma <khushiv@geekyants.com>
2023-10-17 13:38:32 +0530

    ERG component fetched through ssr.

--- src/app/components/Resource/resource.tsx
+++ src/app/components/Resource/resource.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import data from "./resource.json";
 
 interface Resource {
+  image: string;
   title: string;
   description: string;
 }
@@ -17,14 +18,19 @@ export default function Resource() {
   return (
     <div className="2xl:container md:top-[-90px] bg-lightblue  relative top-[200px]">
       <div className=" md:grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 grid-cols-2  gap-8 relative ">
-        <Image
-          src="/Resource.png"
-          alt="Resource ERG Image"
-          className="relative lg:left-[20px] md:mx-auto"
-          width={612}
-          height={350}
-          priority={true}
-        />
+        {resourcedata.map((resource: Resource, index: number) => (
+          <div key={index}>
+            <Image
+              src={resource.image}
+              alt="Resource ERG Image"
+              className="relative lg:left-[20px] md:mx-auto"
+              width={612}
+              height={350}
+              priority={true}
+            />
+          </div>
+        ))}
+
         <div className="flex flex-col p-8 md:mx-auto">
           {resourcedata.map((resource: Resource, index: number) => (
             <div key={index}>
