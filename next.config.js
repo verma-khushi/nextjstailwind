3d09807b80e2c839f5a20c4c771ac17086c92871
Khushi Verma <khushiv@geekyants.com>
2023-10-26 11:59:15 +0530

    Next config changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+         "nextjstailwind-weld.vercel.app"
+        ],
+    }
+      
+}
 
 module.exports = nextConfig
